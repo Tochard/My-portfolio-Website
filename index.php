7f239f8a862b84acfc22dbc5c3bad62874aeb805
Anthony Ebube <egolumaer@gmail.com>
2022-08-18 20:09:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portfolio</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <!-- <header class="header">
+            <nav>
+                <div class="navigations">
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Services</li>
+                        <li>Projects</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </nav>
+        </header> -->
+        <div class="segments">
+
+
+            <div class="side-bar">
+                <div class="profile">
+                    <div class="profile-img">
+                        <img src="./assets/img/profile-4.png" alt="profile-img">
+                    </div>
+                    <div class="desc">
+                        <h4>Ebube Anthony Egolum</h4>
+                        <p>Full-Stack Web Developer</p>
+                    </div>
+                    <div class="social-links">
+                        <ul>
+                            <li>G</li>
+                            <li>L</li>
+                            <li>F</li>
+                            <li>T</li>
+                            <li>I</li>
+                            <li>W</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="main-body">
+                <div class="introduction">
+                    <div class="intro-content">
+                        <div class="intro-text">
+                            <h1>I'm Ebube Anthony Egolum <br> <span> Full-stack</span> Web Developer</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus natus officiis nostrum fuga facere, magni tenetur alias quam voluptatibus ipsa soluta repellendus. Porro, maxime temporibus.</p>
+                        </div>
+                        <div class="intro-img">
+                            <img src="./assets/img/profile-1.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="nav-bar">
+                <div>
+                    da
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</body>
+
+</html>
