69f524cf4d262d9b7a5ab492c5cbd06204e0b5c7
Anthony Ebube <egolumaer@gmail.com>
2022-08-19 11:24:52 +0100

    commit

--- index.php
+++ index.php
@@ -12,19 +12,7 @@
 
 <body>
     <div class="container">
-        <!-- <header class="header">
-            <nav>
-                <div class="navigations">
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Services</li>
-                        <li>Projects</li>
-                        <li>Contact</li>
-                    </ul>
-                </div>
-            </nav>
-        </header> -->
+
         <div class="segments">
 
 
@@ -38,6 +26,7 @@
                         <p>Full-Stack Web Developer</p>
                     </div>
                     <div class="social-links">
+                        <h6>Let's Connect</h6>
                         <a href="#"><i class="fa-brands fa-square-github"></i></a>
                         <a href="#"><i class="fa-brands fa-linkedin"></i></a>
                         <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
@@ -47,19 +36,63 @@
                         </ul>
                     </div>
                 </div>
+
                 <div class="other-details">
-                    <p>Age: <span>22</span></p>
-                    <p>Language: <span>English, Igbo</span></p>
-                    <p>Residence: <span>Awka</span></p>
+                    <!-- <p>Age: <span>22</span></p> -->
+                    <p>Languages: <span>English, Igbo</span></p>
+                    <p>Residence: <span>Anambra</span></p>
                     <p>Country: <span>Nigeria</span></p>
                 </div>
-                <div class="other-details">
+
+                <!-- <div class="other-details">
                     <h4>Skills</h4>
-                    <!-- <p>HTML | CSS | Javascript | PHP </p> -->
+                    <p>HTML | CSS | Javascript | PHP </p>
                     <p>Html <span>90%</span></p>
                     <p>Css <span>90%</span></p>
                     <p>Javascript <span>75%</span></p>
                     <p>Php <span>85%</span></p>
+                </div> -->
+
+                <div class="skills">
+                    <h6>Skills</h6>
+                    <div class="skill">
+                        <div class="skill-name">HTML <span>95%</span></div>
+                        <div class="skill-bar">
+                            <div class="skill-per" per="95%" style="max-width:95%"></div>
+                        </div>
+                    </div>
+
+                    <div class="skill">
+                        <div class="skill-name">CSS <span>85%</span></div>
+                        <div class="skill-bar">
+                            <div class="skill-per" style="max-width:85%"></div>
+                        </div>
+                    </div>
+
+                    <div class="skill">
+                        <div class="skill-name">Javascript <span>60%</span></div>
+                        <div class="skill-bar">
+                            <div class="skill-per" per="60%" style="max-width:60%"></div>
+                        </div>
+                    </div>
+
+                    <div class="skill">
+                        <div class="skill-name">PHP <span>80%</span></div>
+                        <div class="skill-bar">
+                            <div class="skill-per" per="80%" style="max-width:80%"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="extra-skills">
+                    <h6>Extra Skills</h6>
+                    <p><span><i class="fa-solid fa-check"></i></span> WordPress</p>
+                    <p><span><i class="fa-solid fa-check"></i></span> Sass, Bootstrap</p>
+                    <p><span><i class="fa-solid fa-check"></i></span> JQuery, Ajax</p>
+                    <p><span><i class="fa-solid fa-check"></i></span> GIT knowledge</p>
+                </div>
+
+                <div class="cv-download">
+                    <button><a href="#">Download CV <i class="fa-solid fa-download"></i></a></button>
                 </div>
             </div>
 
@@ -69,12 +102,12 @@
                 <div class="introduction">
                     <div class="intro-content">
                         <div class="intro-text">
-                            <h1>I'm Ebube Anthony Egolum <br> <span> Full-stack</span> Web Developer</h1>
+                            <h1>Hi, <br>I'm Ebube Anthony Egolum, <br><span>Full-stack</span> Web Developer</h1>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus natus officiis nostrum fuga facere, magni tenetur alias quam voluptatibus ipsa soluta repellendus. Porro, maxime temporibus.</p>
                             <button><a href="#">Know More <i class="fa-solid fa-arrow-right fa-1x"></i></a></button>
                         </div>
                         <div class="intro-img">
-                            <img src="./assets/img/profile-5.png" alt="">
+                            <img src="./assets/img/profile-.png" alt="">
                         </div>
                     </div>
                 </div>
