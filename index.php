c6979edfd3ec8634b194916e0b282731e8cccd5c
Anthony Ebube <egolumaer@gmail.com>
2022-08-23 04:34:24 +0100

    additional css stylings

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
                         <img src="./assets/img/profile-4.png" alt="profile-img">
                     </div>
                     <div class="desc">
-                        <h4>Ebube Anthony Egolum</h4>
+                        <h4>Ebube Anthony <span>Egolum</span></h4>
                         <p>Full-Stack Web Developer</p>
                     </div>
                     <div class="social-links">
@@ -56,9 +56,9 @@
                 <div class="skills">
                     <h6>Skills</h6>
                     <div class="skill">
-                        <div class="skill-name">HTML <span>95%</span></div>
+                        <div class="skill-name">HTML <span>90%</span></div>
                         <div class="skill-bar">
-                            <div class="skill-per" per="95%" style="max-width:95%"></div>
+                            <div class="skill-per" style="max-width:90%"></div>
                         </div>
                     </div>
 
@@ -72,14 +72,14 @@
                     <div class="skill">
                         <div class="skill-name">Javascript <span>60%</span></div>
                         <div class="skill-bar">
-                            <div class="skill-per" per="60%" style="max-width:60%"></div>
+                            <div class="skill-per" style="max-width:60%"></div>
                         </div>
                     </div>
 
                     <div class="skill">
                         <div class="skill-name">PHP <span>80%</span></div>
                         <div class="skill-bar">
-                            <div class="skill-per" per="80%" style="max-width:80%"></div>
+                            <div class="skill-per" style="max-width:80%"></div>
                         </div>
                     </div>
                 </div>
@@ -92,7 +92,7 @@
                 </div>
 
                 <div class="cv-download">
-                    <button><a href="#">Download CV <i class="fa-solid fa-download"></i></a></button>
+                    <button><a href="#"><i class="fa-solid fa-download"></i> Download CV </a></button>
                 </div>
             </div>
 
@@ -103,21 +103,245 @@
                     <div class="intro-content">
                         <div class="intro-text">
                             <h1>Hi, <br>I'm Ebube Anthony Egolum, <br><span>Full-stack</span> Web Developer</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus natus officiis nostrum fuga facere, magni tenetur alias quam voluptatibus ipsa soluta repellendus. Porro, maxime temporibus.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis et eaque, libero distinctio illo hic suscipit doloribus nostrum temporibus sunt.</p>
                             <button><a href="#">Know More <i class="fa-solid fa-arrow-right fa-1x"></i></a></button>
                         </div>
                         <div class="intro-img">
-                            <img src="./assets/img/profile-.png" alt="">
+                            <img src="./assets/img/profile-5.png" alt="">
                         </div>
                     </div>
+                </div>
+
+                <div class="about section">
+                    <h4>ABOUT ME</h4>
+                    <h6>Know More About Me</h6>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae numquam fugit vero praesentium, exercitationem culpa unde aliquid qui consectetur aliquam dolor perferendis cupiditate voluptate excepturi optio voluptatem reprehenderit neque, sunt non quas! Voluptates, consequatur facilis culpa iusto</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloremque, exercitationem sequi eaque fugiat debitis asperiores, molestias itaque, eos amet quos nihil sint suscipit vero perspiciatis iure omnis similique temporibus?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptates tempora porro cumque harum eum?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil, labore. At sint delectus eligendi aliquam, tempore quisquam? Laudantium nam incidunt nostrum expedita vero porro, velit facilis ut eveniet iste! Doloribus voluptatibus distinctio aut adipisci maiores reiciendis incidunt, rerum quasi impedit.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloremque, exercitationem sequi eaque fugiat debitis asperiores, molestias itaque, eos amet quos nihil sint suscipit vero perspiciatis iure omnis similique temporibus?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptates tempora porro cumque harum eum?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil, labore. At sint delectus eligendi aliquam, tempore quisquam? Laudantium nam incidunt nostrum expedita vero porro, velit facilis ut eveniet iste! Doloribus voluptatibus distinctio aut adipisci maiores reiciendis incidunt, rerum quasi impedit.</p>
+                </div>
+
+                <div class="resume section">
+                    <h4>MY RESUME</h4>
+                    <!-- <h6>Know More About Me</h6> -->
+                    <br>
+                    <br>
+                    <br>
+                    <div class="resume-details">
+                        <div>
+                            <p><span>Address:</span> Awka, Anambra State, Nigeria.</p>
+                            <p><span>Phone:</span> +2348169021239</p>
+                            <p><span>Email:</span> egolumaer@gmail.com</p>
+                        </div>
+                        <div>
+                            <p><span>GitHub:</span> www.github.com/Tochard</p>
+                            <p><span>Linkedin:</span> www.linkedin.com/egolumanthony/</p>
+                        </div>
+                    </div>
+
+                    <br>
+                    <br>
+
+                    <h6>FULL-STACK SOFTWARE ENGINEER</h6>
+                    <br>
+                    <p>Self taught Software Engineer student with a background in Engineering. Possess strong skills in problem solving, communication and collaboration that leads to quality products and results. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda minima tempora consequuntur sunt placeat debitis ducimus impedit, quia suscipit eius quae quasi dolores sequi praesentium dolore error officia qui nihil non nam vero accusamus illo. Vel ad consequuntur modi natus.</p>
+
+                    <br>
+                    <br>
+
+                    <h6>TECHNICAL SKILLS</h6>
+                    <br>
+                    <div class="resume-details">
+                        <div>
+                            <p><span>Languages:</span> PHP,Javascript, CSS, HTML</p>
+                            <p><span>Databases:</span> SQL, MySQL</p>
+                        </div>
+                        <div>
+                            <p><span>Version Control:</span> Git, GitHub</p>
+                            <p><span>Tools:</span> VS Code, PhpStorm, XAMPP</p>
+                        </div>
+                    </div>
+
+                    <br>
+                    <br>
+
+                    <h6>TECHNICAL PROJECTS</h6>
+                    <br>
+                    <p><span>Web-Based Chat Application</span>- GitHub: github.com/Tochard/Web-Based-Chat-Application | Live Site: https://web-based-chat-application-1.herokuapp.com/</p>
+                    <p>A chat web application allows signed up users to message each other.</p>
+                    <p>
+                        <i class="fa-solid fa-check"></i> Used MySQL, SQL and PhpMyadmin to create a database in the backend.
+                        <br>
+                        <i class="fa-solid fa-check"></i> Used Ajax to get data from the database.
+                        <br>
+                        <i class="fa-solid fa-check"></i> Used php time function and session to automatically logout idle users.
+                        <br>
+                        <i class="fa-solid fa-check"></i> Engineered controlled forms for user login, sign-up, user recommendations and feedback messages.
+                        <br>
+                        <i class="fa-solid fa-check"></i> Used CSS for styling.
+                        <br>
+                        <i class="fa-solid fa-check"></i> Deployed with Heroku.
+                    </p>
+                    <br>
+                    <p><span>Courier Website</span> - GitHub: github.com/Tochard/Courier-Website | Live Site: https://courier-website-1.herokuapp.com/</p>
+                    <p>A courier website where admin can register packages for delivery and clients using a unique tracking ID can track their package.</p>
+                    <p>
+                        <i class="fa-solid fa-check"></i> Used MySQL, SQL and PhpMyadmin to create a database in the backend.
+                        <br>
+                        <i class="fa-solid fa-check"></i> Used php uniqid function to generate tracking ID.
+                        <br>
+                        <i class="fa-solid fa-check"></i> Used CSS for styling.
+                        <br>
+                        <i class="fa-solid fa-check"></i> Deployed with Heroku.
+                    </p>
+                    <br>
+                    <p><span>Simple Shopping Cart</span>- GitHub: github.com/Tochard/Simple-Shopping-Cart</p>
+                    <p>A simple shopping cart, it has a simple admin dashboard to add items . It also allows users to add products to their cart for processing and payments.</p>
+                    <p>
+                        <i class="fa-solid fa-check"></i> Used MySQL, SQL and PhpMyadmin to create a database in the backend.
+                        <br>
+                        <i class="fa-solid fa-check"></i> Used Php session function to display the number of items in the cart.
+                        <br>
+                        <i class="fa-solid fa-check"></i> Used CSS for styling.
+                    </p>
+
+                    <br>
+                    <br>
+
+                    <h6>PROFESSIONAL EXPERIENCE</h6>
+                    <br>
+                    <p>
+                        <i class="fa-solid fa-check"></i> Applied computer science data structures and algorithms to solve computational problems ensuring the data flow within a finite amount of space and time.
+                        <br>
+                        <i class="fa-solid fa-check"></i> Gained hands-on experience with PHP, JavaScript, CSS, HTML, MySQL and tech principles whilst working on team/individual projects, code reviews, and pair programming sessions.
+                    </p>
+
+                    <br>
+                    <br>
+
+                    <h6>EDUCATION</h6>
+                    <br>
+                    <div class="resume-details">
+                        <div>
+                            <p><span>Nnamdi Azikiwe University</span></p>
+                            <p><span></span>Undergraduate (Mechanical Engineering)</p>
+                        </div>
+                        <div>
+                            <p><span></span>Anambra State, Nigeria</p>
+                            <p><span></span>Expected Graduation: December, 2022</p>
+                        </div>
+                    </div>
+
+                    <br>
+                    <br>
+
+                </div>
+
+                <div class="portfolio section">
+                    <h4>Portfolio</h4>
+                    <h6>Some Recent Projects</h6>
+
+                    <div class="projects">
+
+                        <div class="project">
+
+                        </div>
+                        <div class="project">
+
+                        </div>
+                        <div class="project">
+
+                        </div>
+                        <div class="project">
+
+                        </div>
+                        <div class="project">
+
+                        </div>
+                        <div class="project">
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="testimonials section">
+                    <h4>Recommendation</h4>
+                    <h6>Some Testimonials</h6>
+                    <div class="testi">
+
+                        <div class="test">
+                            <div class="test-text">
+                                <i class="fa-solid fa-check"></i>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis qui neque officia deserunt. Libero veritatis, distinctio nisi quisquam iusto quod placeat, rerum quos explicabo deserunt nostrum</p>
+                                <h6>Lorem ipsum dolor</h6>
+                            </div>
+                            <div class="test-img">
+                                <img src="#" alt="Img">
+                            </div>
+                        </div>
+
+                        <div class="test">
+                            <div class="test-text">
+                                <i class="fa-solid fa-check"></i>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis qui neque officia deserunt. Libero veritatis, distinctio nisi quisquam iusto quod placeat, rerum quos explicabo deserunt nostrum </p>
+                                <h6>Lorem ipsum dolor</h6>
+                            </div>
+                            <div class="test-img">
+                                <img src="#" alt="Img">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="contact section">
+                    <h4>Contact Us</h4>
+                    <h6>Get In Touch With Us</h6>
+                    <div class="cont">
+                        <div class="cont-detail">
+                            <h6>cotact info</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, dolor!</p>
+                        </div>
+                        <div class="cont-form">
+                            <form action="#">
+                                <input type="text" name="name" class="input" placeholder="Name" required>
+                                <input type="text" name="email" class="input" placeholder="Email" required>
+                                <input type="text" name="subject" class="input" placeholder="Subject" required>
+                                <textarea name="message" class="textarea" placeholder="Message" required></textarea>
+                                <button name="send">Send Mail</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="footer">
+                    <footer>
+                        <p> &copy; 2022 All Rights Reserved. <span>Email: egolumaer@gmail.com</span></p>
+                    </footer>
                 </div>
             </div>
 
 
 
             <div class="nav-bar">
-                <div>
-                    <p>fdk</p>
+                <div class="mood">
+                    <i class="fa-solid fa-toggle-off"></i>
+                    <!-- <i class="fa-solid fa-sun"></i> -->
+                    <!-- <i class="fa-solid fa-moon"></i> -->
+                </div>
+
+                <div class="navs">
+                    <div class='nav'><a href="#"><i class="fa-solid fa-house"></i></a></div>
+                    <div class='nav'><a href="#"><i class="fa-solid fa-user"></i></a></div>
+                    <div class='nav'><a href="#"><i class="fa-solid fa-file-lines"></i></a></div>
+                    <div class='nav'><a href="#"><i class="fa-solid fa-laptop-code"></i></a></div>
+                    <div class='nav'><a href="#"><i class="fa-solid fa-folder"></i></a></div>
+                    <div class='nav'><a href="#"><i class="fa-solid fa-comment"></i></a></div>
+                    <div class='nav'><a href="#"><i class="fa-solid fa-address-card"></i></a></div>
+
                 </div>
             </div>
 
